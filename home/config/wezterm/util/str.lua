3fb173f896dff3db4e58eaa030e69864413dc056
Max Weber <mweber@kzvnr.de>
2024-10-07 09:47:21 +0200

    fix wezterm config

--- home/config/wezterm/util/str.lua
+++ home/config/wezterm/util/str.lua
@@ -1,1 +1,20 @@
-/home/mweber/.config/dotbot/home/config/wezterm/str.lua
+local string = require "string"
+
+return {
+    trim = function (s) 
+        return (s:gsub("^%s*(.-)%s*$", "%1"))
+    end,
+    startsWith = function(s, with)
+        return string.sub(s,1,string.len(with)) == with
+    end,
+    lines=function(s)
+        if s == nil then 
+            return {} 
+        end
+        return s:gmatch("[^\n]+")
+    end,
+    count_lines = function(s)
+        return select(2, s:gsub('\n', '\n')) +1
+    end
+
+}
